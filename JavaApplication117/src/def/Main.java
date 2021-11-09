e2a318360179953e3e960f228478274af9c41b04
FALCONE <FALCONE@PC-ROBERTO>
2021-11-09 17:21:43 +0100

    Main

---
+++ JavaApplication117/src/def/Main.java
@@ -0,0 +1,12 @@
+/*  Main.java
+    9-nov-2021
+    17.21.16
+    FALCONE     */
+package def;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD!");
+    }
+}
